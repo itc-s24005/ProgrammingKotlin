30b7055588756aa515f51d94cb8af491f58f98e2
itc-s24005 <s24005@std.it-college.ac.jp>
2025-06-04 16:59:25 +0900

    追加分

---
+++ src/chap39/code39_4.kt
@@ -0,0 +1,15 @@
+package chap39
+
+fun main() {
+    val list = listOf("あ", "い", "う", "え", "お")
+    val action: (String) -> Unit = fun(x) {
+        println(x)
+    }
+    list.forEach(action)
+
+    list.forEach { println(it) }
+
+    for (it in list) {
+        println(it)
+    }
+}
